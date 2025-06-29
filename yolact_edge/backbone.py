b2c0aa79ccf944432464ccb3d855ab3f4bf6e13c
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2025-06-29 22:27:42 +0900

    Update backbone.py

--- yolact_edge/backbone.py
+++ yolact_edge/backbone.py
@@ -131,7 +131,7 @@ class ResNetBackbone(nn.Module):
 
     def init_backbone(self, path):
         """ Initializes the backbone weights for training. """
-        state_dict = torch.load(path, map_location='cpu')
+        state_dict = torch.load(path, map_location='cpu', weights_only=True)
 
         # Replace layer1 -> layers.0 etc.
         keys = list(state_dict)
